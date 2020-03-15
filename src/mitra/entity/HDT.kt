e0f0a88ceb64e037ba6ee9f69e256e453f2cbcf2
Takenouchi Keita <t.keita.osaka@gmail.com>
2020-03-15 13:36:39 +0900

    Nodes in HDT have parent nodes.

--- src/mitra/entity/HDT.kt
+++ src/mitra/entity/HDT.kt
@@ -4,10 +4,20 @@ package mitra.entity
  * Hierarchical Data Tree
  */
 sealed class HDT(val tag: String) {
+    var parent: HDNode? = null
+
     abstract fun dumpTree(): String
 }
 
-open class HDNode(tag: Tag, vararg val children: HDT) : HDT(tag) {
+class HDNode(tag: Tag, vararg val children: HDT) : HDT(tag) {
+
+    init {
+        // register the parent of children
+        for (c in children) {
+            c.parent = this
+        }
+    }
+
     override fun dumpTree(): String {
         val ret = StringBuilder()
         ret.append("<$tag>\n")
@@ -15,7 +25,7 @@ open class HDNode(tag: Tag, vararg val children: HDT) : HDT(tag) {
         for (c in children.dropLast(1)) {
             ret.append("├──" + c.dumpTree().replace("\n", "\n│  ") + "\n")
         }
-        
+
         run {
             val c = children.last()
             ret.append("└──" + c.dumpTree().replace("\n", "\n   "))
